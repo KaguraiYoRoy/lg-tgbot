c68e27cfb0043117cad51bce50384737cf2082aa
KaguraiYoRoy <yorisocute233@gmail.com>
2025-07-17 17:41:04 +0800

    master: Add support for UUID auth

--- src/lg-tgbot.cpp
+++ src/lg-tgbot.cpp
@@ -26,6 +26,15 @@ int main(){
     printf("Token: %s\n", token.c_str());
     const bool debugmode=(configRoot.isMember("debug")&&configRoot["debug"].isBool())?configRoot["debug"].asBool():false;
 
+    for(unsigned int i=0;i<configRoot["nodes"].size();i++){
+        if(!configRoot["nodes"][i].isMember("uuid") || !configRoot["nodes"][i]["uuid"].isString()){
+            if(!configRoot["nodes"][i].isMember("name") || !configRoot["nodes"][i]["name"].isString())
+                printf("Config parse error: Node %s does not have UUID!\n",configRoot["nodes"][i]["name"].asCString());
+            else printf("Config parse error: Node %d does not have UUID!\n",i);
+            return 1;
+        }
+    }
+
     TgBot::Bot bot(token);
     bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
         bot.getApi().sendMessage(message->chat->id, "Manual:\n/ping <host>: Ping target host\n/tcping <host> <port>: TCPing host:port\n/trace <host>: Traceroute target host\n/route <host>: Get route for target host\n");
@@ -45,10 +54,10 @@ int main(){
             resstr+="Node: "+configRoot["nodes"][i]["name"].asString()+"\n";
             std::string weburl=configRoot["nodes"][i]["url"].asString();
             httplib::Client cli(weburl.c_str());
-
+        
             if(debugmode)resstr+="Debug info:\n"+weburl+params+"\n";
 
-            auto res = cli.Get(params.c_str());
+            auto res = cli.Get(((std::string)(params+"&uuid="+configRoot["nodes"][i]["uuid"].asString())).c_str());
             if(res->status!=httplib::StatusCode::OK_200){
                 resstr+="Test failed.\nServer returned an error.\n";
             }
@@ -87,7 +96,7 @@ int main(){
 
             if(debugmode)resstr+="Debug info:\n"+weburl+params+"\n";
 
-            auto res = cli.Get(params.c_str());
+            auto res = cli.Get(((std::string)(params+"&uuid="+configRoot["nodes"][i]["uuid"].asString())).c_str());
             if(res->status!=httplib::StatusCode::OK_200){
                 resstr+="Test failed.\nServer returned an error.\n";
             }
@@ -127,7 +136,7 @@ int main(){
 
             if(debugmode)resstr+="Debug info:\n"+weburl+params+"\n";
 
-            auto res = cli.Get(params.c_str());
+            auto res = cli.Get(((std::string)(params+"&uuid="+configRoot["nodes"][i]["uuid"].asString())).c_str());
             if(res->status!=httplib::StatusCode::OK_200){
                 resstr+="Test failed.\nServer returned an error.\n";
             }
@@ -163,10 +172,10 @@ int main(){
             resstr+="Node: "+configRoot["nodes"][i]["name"].asString()+"\n";
             std::string weburl=configRoot["nodes"][i]["url"].asString();
             httplib::Client cli(weburl.c_str());
-
+        
             if(debugmode)resstr+="Debug info:\n"+weburl+params+"\n";
-
-            auto res = cli.Get(params.c_str());
+            
+            auto res = cli.Get(((std::string)(params+"&uuid="+configRoot["nodes"][i]["uuid"].asString())).c_str());
             if(res->status!=httplib::StatusCode::OK_200){
                 resstr+="Test failed.\nServer returned an error.\n";
             }
